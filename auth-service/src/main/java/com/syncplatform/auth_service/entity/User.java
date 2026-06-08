15e04e65192489286d5060e24f7c8d01ebb90b49
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-09 01:02:41 +0530

    Add JdbcTypeCode annotation for UserStatus enum in User entity

--- auth-service/src/main/java/com/syncplatform/auth_service/entity/User.java
+++ auth-service/src/main/java/com/syncplatform/auth_service/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "users")
@@ -24,6 +26,7 @@ public class User {
     private String fullName;
 
     @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
     @Column(columnDefinition = "user_status")
     private UserStatus status;
 
