eb31c9622d8f1029844cb96476277accc0fef019
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-09 01:06:50 +0530

    Add spring initalizer generated subscription-service

---
+++ subscription-service/src/test/java/com/syncplatform/subscription_service/SubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncplatform.subscription_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
