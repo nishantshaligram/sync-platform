840ba30cedb1286e6b92f95198e0447498dbbb1a
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-11 18:01:28 +0530

    Add spring boot intializer generated sync-core-service project boilerplate

---
+++ sync-core-service/src/main/java/com/syncplatform/sync_core_service/SyncCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syncplatform.sync_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncCoreServiceApplication.class, args);
+	}
+
+}
---
+++ sync-core-service/src/test/java/com/syncplatform/sync_core_service/SyncCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncplatform.sync_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
