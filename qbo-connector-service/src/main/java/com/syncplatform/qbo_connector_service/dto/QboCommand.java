a5a308f5437c8f2fcb059211349c5dbf492d934c
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-11 15:59:53 +0530

    Add QboCommand DTO for handling QBO command data

---
+++ qbo-connector-service/src/main/java/com/syncplatform/qbo_connector_service/dto/QboCommand.java
@@ -0,0 +1,17 @@
+package com.syncplatform.qbo_connector_service.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class QboCommand {
+    private String commandId;
+    private String syncRunId;
+    private String syncConnectionId;
+    private String commandType;
+    private String canonicalEntityId;
+    private String canonicalEntityType;
+    private List<String> dependsOn;
+    private String issuedAt;
+    private Object payload;
+}
