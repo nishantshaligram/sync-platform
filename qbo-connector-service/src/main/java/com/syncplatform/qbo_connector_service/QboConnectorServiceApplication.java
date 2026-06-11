0f5dae82869ac189ae74b9142e7b1acc0bbab7ee
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-11 15:45:49 +0530

    Add spring boot initializer generated project

---
+++ qbo-connector-service/src/main/java/com/syncplatform/qbo_connector_service/QboConnectorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syncplatform.qbo_connector_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QboConnectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QboConnectorServiceApplication.class, args);
+	}
+
+}
