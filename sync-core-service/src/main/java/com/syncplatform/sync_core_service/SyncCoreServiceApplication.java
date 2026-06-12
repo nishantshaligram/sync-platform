da21728eb2b80a2cb99b6f95a6f999ce37e1ae6f
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-12 18:05:06 +0530

    Add GlobalExceptionHandler to manage exceptions and provide structured error responses

--- sync-core-service/src/main/java/com/syncplatform/sync_core_service/SyncCoreServiceApplication.java
+++ sync-core-service/src/main/java/com/syncplatform/sync_core_service/SyncCoreServiceApplication.java
@@ -2,8 +2,10 @@ package com.syncplatform.sync_core_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SyncCoreServiceApplication {
 
 	public static void main(String[] args) {
