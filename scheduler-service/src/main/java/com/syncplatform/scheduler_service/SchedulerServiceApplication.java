1c3b70021b23ad12824c410ab2af4b304e09ee0d
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-12 23:33:35 +0530

    Enable discovery client and scheduling in SchedulerServiceApplication

--- scheduler-service/src/main/java/com/syncplatform/scheduler_service/SchedulerServiceApplication.java
+++ scheduler-service/src/main/java/com/syncplatform/scheduler_service/SchedulerServiceApplication.java
@@ -2,8 +2,12 @@ package com.syncplatform.scheduler_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 public class SchedulerServiceApplication {
 
 	public static void main(String[] args) {
