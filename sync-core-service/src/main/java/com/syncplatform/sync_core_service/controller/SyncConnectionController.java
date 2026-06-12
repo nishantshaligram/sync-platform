94af7b1cf069058825d8280d7202e963112f109b
nishantshaligram <nishant.shaligram@gmail.com>
2026-06-12 17:56:12 +0530

    Add SyncConnection Controller to handle connection create, list and retrieval

---
+++ sync-core-service/src/main/java/com/syncplatform/sync_core_service/controller/SyncConnectionController.java
@@ -0,0 +1,47 @@
+package com.syncplatform.sync_core_service.controller;
+
+import com.syncplatform.sync_core_service.dto.*;
+import com.syncplatform.sync_core_service.service.SyncConnectionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/connections")
+@RequiredArgsConstructor
+public class SyncConnectionController {
+
+    private final SyncConnectionService syncConnectionService;
+
+    @PostMapping
+    public ResponseEntity<ConnectionResponse> create(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestBody CreateConnectionRequest request) {
+
+        UUID userId = UUID.fromString(jwt.getSubject());
+        return ResponseEntity.ok(
+                syncConnectionService.createConnection(userId, request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ConnectionResponse>> list(
+            @AuthenticationPrincipal Jwt jwt) {
+
+        UUID userId = UUID.fromString(jwt.getSubject());
+        return ResponseEntity.ok(syncConnectionService.listConnections(userId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ConnectionResponse> get(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID id) {
+
+        UUID userId = UUID.fromString(jwt.getSubject());
+        return ResponseEntity.ok(syncConnectionService.getConnection(userId, id));
+    }
+}
